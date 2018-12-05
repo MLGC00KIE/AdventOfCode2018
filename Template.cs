b4dd5cb0d753e8ede69c280cb6b5991db6e08ee0
MLGC00KIE <24893@ma-web.nl>
2018-12-05 20:43:43 +0100

    catching up - day3.1

---
+++ Template.cs
@@ -0,0 +1,36 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Part1());
+            Console.WriteLine(Part2());
+            Console.ReadLine();
+        }
+
+        static int Part1()
+        {
+            int flag = 0;
+
+
+
+            return flag;
+        }
+
+        static int Part2()
+        {
+            int flag = 0;
+
+
+
+            return flag;
+        }
+    }
+}
